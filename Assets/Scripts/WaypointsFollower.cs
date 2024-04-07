ba350f3fa0b20e9dc5354654346d930c6c2565b6
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-04-07 23:10:14 +0200

    waypoint follower show path in editor

--- Assets/Scripts/WaypointsFollower.cs
+++ Assets/Scripts/WaypointsFollower.cs
@@ -9,7 +9,15 @@ public class WaypointsFollower : MonoBehaviour
 
     int nextWaypoint = 0;
 
-    void Update()
+    private void Start()
+    {
+        if (GetComponent<Rigidbody>() != null)
+        {
+            movingSpeed *= 4;
+        }
+    }
+
+    private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, waypoints[nextWaypoint].position, movingSpeed * Time.deltaTime);
         if (Vector3.Distance(transform.position, waypoints[nextWaypoint].position) < 0.1f)
@@ -20,5 +28,15 @@ public class WaypointsFollower : MonoBehaviour
         {
             nextWaypoint = 0;
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        for (int i = 0; i < waypoints.Length - 1; i++)
+        {
+            Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
+        }
+        Gizmos.DrawLine(waypoints[waypoints.Length - 1].position, waypoints[0].position);
     }
 }
