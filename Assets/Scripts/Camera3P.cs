00fe932adbfc7b347981d81aa21b81f7c6d3167b
Gaikk <gaik.k02@gmail.com>
2024-05-15 12:58:33 +0200

    changed aiming from lshift to right click

--- Assets/Scripts/Camera3P.cs
+++ Assets/Scripts/Camera3P.cs
@@ -37,7 +37,7 @@ public class Camera3P : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             aim = !aim;
             
