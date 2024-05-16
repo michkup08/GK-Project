3284193e81dde34569cde7359ba10994181b0805
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-05-16 23:36:58 +0200

    Level4WaterReset, Level5LavaReset scripts comments

---
+++ Assets/Scripts/Level4WaterReset.cs
@@ -0,0 +1,82 @@
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// The Level4WaterReset class is responsible for resetting the player's position when the player is underwater.
+/// It checks if the player is underwater and if so, starts a countdown.
+/// If the player stays underwater for a certain amount of time, the player's position is reset.
+/// </summary>
+public class Level4WaterReset : MonoBehaviour
+{
+    /// <value><c>waterLevel</c> is the y-coordinate of the water's surface.</value>
+    private double waterLevel;
+
+    /// <value><c>playerHeight</c> is the height of the player.</value>
+    private double playerHeight;
+
+    /// <value><c>underWaterText</c> is the TextMeshPro component where the countdown is displayed.</value>
+    [SerializeField]
+    private TMP_Text underWaterText;
+
+    /// <value><c>player</c> is the player's GameObject.</value>
+    private GameObject player;
+
+    /// <value><c>startPoint</c> is the Transform where the player is reset to after falling into the water.</value>
+    [SerializeField]
+    private Transform startPoint;
+
+    /// <value><c>numberOfSecondsToWait</c> is the number of seconds to wait before resetting the player's position.</value>
+    [SerializeField]
+    private float numberOfSecondsToWait = 5;
+
+    /// <value><c>timeElapsed</c> is the time in seconds that has elapsed since the player fell into the water.</value>
+    private float timeElapsed = 0;
+
+    /// <value><c>secondsElapsed</c> is the number of whole seconds that has elapsed since the player fell into the water.</value>
+    private int secondsElapsed = 0;
+
+    /// <summary>
+    /// Initialization of player, playerHeight, and waterLevel variables.
+    /// </summary>
+    void Start()
+    {
+        waterLevel = GameObject.Find("Water Specular").transform.position.y;
+        player = GameObject.Find("Player");
+        playerHeight = player.transform.Find("PlayerObject").GetComponent<CapsuleCollider>().height;
+    }
+
+    /// <summary>
+    /// Checks if the player has fallen into the water and if so, starts a countdown to reset the player's position.
+    /// </summary>
+    void Update()
+    {
+        if (waterLevel > (player.transform.position.y + playerHeight / 2) + 0.1)
+        {
+            timeElapsed += Time.deltaTime;
+            secondsElapsed = (int)timeElapsed % 60;
+            underWaterText.text = "Underwater!\n" + (numberOfSecondsToWait - secondsElapsed) + "\nseconds to reset";
+            underWaterText.gameObject.SetActive(true);
+            if (timeElapsed >= numberOfSecondsToWait)
+            {
+                ResetPlayerPosition();
+                timeElapsed = 0;
+            }
+        }
+        else
+        {
+            underWaterText.gameObject.SetActive(false);
+            timeElapsed = 0;
+        }
+    }
+
+    /// <summary>
+    /// The ResetPlayerPosition method resets the player's position to the startPoint and stops any movement.
+    /// </summary>
+    private void ResetPlayerPosition()
+    {
+        var rigidBody = player.GetComponent<Rigidbody>();
+        rigidBody.velocity = Vector3.zero;
+        player.transform.position = startPoint.position;
+        rigidBody.MovePosition(startPoint.position);
+    }
+}
--- Assets/Scripts/Level5LavaReset.cs
+++ Assets/Scripts/Level5LavaReset.cs
@@ -1,40 +1,67 @@
 using TMPro;
+
 using UnityEngine;
 
-public class Level4WaterResetPlayerPosition : MonoBehaviour
+/// <summary>
+/// The Level5LavaReset class is responsible for resetting the player's position when the player fell into the lava.
+/// It checks if the player fell into lava and if so, starts a countdown.
+/// After a certain amount of time, the player's position is reset.
+/// </summary>
+public class Level5LavaReset : MonoBehaviour
 {
-    private double waterLevel;
+    /// <value><c>lavaLevel</c> is the y-coordinate of the lava's surface.</value>
+    private double lavaLevel;
+
+    /// <value><c>playerHeight</c> is the height of the player.</value>
     private double playerHeight;
 
+    /// <value><c>textGUI</c> is the TextMeshPro component where the countdown is displayed.</value>
     [SerializeField]
-    private TMP_Text underWaterText;
+    private TMP_Text textGUI;
 
+    /// <value><c>player</c> is the player's GameObject.</value>
     private GameObject player;
 
+    /// <value><c>startPoint</c> is the Transform where the player is reset to after falling into the lava.</value>
     [SerializeField]
     private Transform startPoint;
 
+    /// <value><c>numberOfSecondsToWait</c> is the number of seconds to wait before resetting the player's position.</value>
     [SerializeField]
     private float numberOfSecondsToWait = 5;
 
+    /// <value><c>lavaObject</c> is the GameObject of the lava.</value>
+    [SerializeField]
+    private GameObject lavaObject;
+
+    /// <value><c>timeElapsed</c> is the time in seconds that has elapsed since the player fell into the lava.</value>
     private float timeElapsed = 0;
+
+    /// <value><c>secondsElapsed</c> is the number of whole seconds that has elapsed since the player fell into the lava.</value>
     private int secondsElapsed = 0;
 
+    /// <summary>
+    /// The Start method is called before the first frame update. It initializes the player, playerHeight, and lavaLevel variables.
+    /// </summary>
     void Start()
     {
-        waterLevel = GameObject.Find("Water Specular").transform.position.y;
         player = GameObject.Find("Player");
-        playerHeight = player.transform.Find("PlayerObject").GetComponent<CapsuleCollider>().height;
+        playerHeight = player.transform.position.y;
+        lavaLevel = lavaObject.transform.position.y;
     }
 
+    /// <summary>
+    /// The Update method is called once per frame. It checks if the player has fallen into the lava and if so, starts a countdown to reset the player's position.
+    /// </summary>
     void Update()
     {
-        if (waterLevel > (player.transform.position.y + playerHeight / 2) + 0.1)
+        if (player.transform.position.y - playerHeight / 2 - 0.2f <= lavaLevel)
         {
+            player.transform.Find("PlayerObject/PlayerFire").gameObject.SetActive(true);
             timeElapsed += Time.deltaTime;
             secondsElapsed = (int)timeElapsed % 60;
-            underWaterText.text = "Underwater!\n" + (numberOfSecondsToWait - secondsElapsed) + "\nseconds to reset";
-            underWaterText.gameObject.SetActive(true);
+            textGUI.text = (numberOfSecondsToWait - secondsElapsed) + "\nseconds to reset";
+            textGUI.gameObject.SetActive(true);
             if (timeElapsed >= numberOfSecondsToWait)
             {
                 ResetPlayerPosition();
@@ -43,13 +70,17 @@ public class Level4WaterResetPlayerPosition : MonoBehaviour
         }
         else
         {
-            underWaterText.gameObject.SetActive(false);
+            textGUI.gameObject.SetActive(false);
             timeElapsed = 0;
         }
     }
 
+    /// <summary>
+    /// The ResetPlayerPosition method resets the player's position to the startPoint and stops any movement.
+    /// </summary>
     private void ResetPlayerPosition()
     {
+        player.transform.Find("PlayerObject/PlayerFire").gameObject.SetActive(false);
         var rigidBody = player.GetComponent<Rigidbody>();
         rigidBody.velocity = Vector3.zero;
         player.transform.position = startPoint.position;
