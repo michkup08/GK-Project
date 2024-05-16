6c45b33937079ced7f30488a0ff49fe81983bc76
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-05-16 23:13:11 +0200

    ScreenHints script comments

--- Assets/Scripts/ScreenHints.cs
+++ Assets/Scripts/ScreenHints.cs
@@ -1,24 +1,38 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
+
 using UnityEngine;
 
+/// <summary>
+/// The ScreenHints class handles the display of on-screen messages.
+/// </summary>
 public class ScreenHints : MonoBehaviour
 {
+    /// <value><c>messages</c> is an array of strings that form the display message.</value>
     private string[] messages;
 
+    /// <value><c>timeToDisplay</c> is the time in seconds that the message is displayed.</value>
     public double timeToDisplay = 3.0;
+
+    /// <value><c>timeDisplaying</c> is the time in seconds that the current message has been displayed.</value>
     private double timeDisplaying = 0.0;
 
+    /// <value><c>messageShown</c> indicates whether the message has been shown.</value>
     private bool messageShown = false;
+
+    /// <value><c>isDisplaying</c> indicates whether any message is currently being displayed.</value>
     private bool isDisplaying = false;
 
+    /// <value><c>loadedMessages</c> is a set of the names of the messages that have been loaded.</value>
     private HashSet<string> loadedMessages = new HashSet<string>();
 
+    /// <value><c>canvasText</c> is the TextMeshPro component where the messages are displayed.</value>
     [SerializeField]
     private TMP_Text canvasText;
 
+    /// <summary>
+    /// Handling the display and removal of messages.
+    /// </summary>
     void Update()
     {
         if (isDisplaying)
@@ -47,6 +61,11 @@ public class ScreenHints : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// The LoadMessage method loads a message to be displayed.
+    /// </summary>
+    /// <param name="messages">The message to display.</param>
+    /// <param name="name">The name of the message.</param>
     public void LoadMessage(string[] messages, string name)
     {
         if (isDisplaying)
