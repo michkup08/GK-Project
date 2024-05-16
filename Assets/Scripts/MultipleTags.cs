7cf801d9f22ed2d02db7e69e07ae6b84de646e67
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-05-16 23:10:14 +0200

    MultipleTags script comments

--- Assets/Scripts/MultipleTags.cs
+++ Assets/Scripts/MultipleTags.cs
@@ -1,14 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The MultipleTags class allows a GameObject to have multiple tags.
+/// </summary>
 public class MultipleTags : MonoBehaviour
 {
+    /// <value><c>tags</c> is a list of tags assigned to the GameObject.</value>
     [SerializeField]
     private List<string> tags;
 
+    /// <summary>
+    /// Checks if a specific tag is assigned to the GameObject.
+    /// </summary>
+    /// <param name="tag">The tag to check.</param>
+    /// <returns>True if the tag is assigned to the GameObject, false otherwise.</returns>
     public bool HasTag(string tag) => tags.Contains(tag);
 
+    /// <summary>
+    /// Adds a tag to the GameObject.
+    /// </summary>
+    /// <param name="tag">The tag to add.</param>
     public void AddTag(string tag)
     {
         if (!tags.Contains(tag))
@@ -17,6 +29,10 @@ public class MultipleTags : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Removes a tag from the GameObject.
+    /// </summary>
+    /// <param name="tag">The tag to remove.</param>
     public void RemoveTag(string tag)
     {
         if (tags.Contains(tag))
@@ -25,5 +41,9 @@ public class MultipleTags : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Returns the list of tags assigned to the GameObject.
+    /// </summary>
+    /// <returns>The list of tags.</returns>
     public List<string> GetTags() => tags;
 }
