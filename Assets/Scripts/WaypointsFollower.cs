5c9f87e1d7df0b671fb7d0dbb409d2956962fd01
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-05-16 23:03:19 +0200

    WaypointsFollower script comments

--- Assets/Scripts/WaypointsFollower.cs
+++ Assets/Scripts/WaypointsFollower.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 
+/// <summary>
+/// The WaypointsFollower class handles the movement of an object along a set of waypoints.
+/// </summary>
 public class WaypointsFollower : MonoBehaviour
 {
+    /// <value><c>movingSpeed</c> is the speed at which the object moves between waypoints.</value>
     [SerializeField]
     private float movingSpeed;
+
+    /// <value><c>waypoints</c> is an array of Transform objects that the object will move between.</value>
     [SerializeField]
     private Transform[] waypoints;
 
-    int nextWaypoint = 0;
+    /// <value><c>nextWaypoint</c> is the index of the next waypoint in the waypoints array that the object will move towards.</value>
+    private int nextWaypoint = 0;
 
+    /// <summary>
+    /// The Start method is called before after game starts. It checks if the object has a Rigidbody component and if so, multiplies the movingSpeed by 4.
+    /// </summary>
     private void Start()
     {
         if (GetComponent<Rigidbody>() != null)
@@ -17,6 +27,9 @@ public class WaypointsFollower : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// The Update method called once per frame. It moves the object towards the next waypoint and updates the nextWaypoint index when the object reaches a waypoint.
+    /// </summary>
     private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, waypoints[nextWaypoint].position, movingSpeed * Time.deltaTime);
@@ -30,6 +43,9 @@ public class WaypointsFollower : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Method to draw lines between waypoints in the Scene view to help visualize the path the object will take.
+    /// </summary>
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
