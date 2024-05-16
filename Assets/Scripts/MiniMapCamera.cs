e628984bc9a622c8cd05f1c8c2c85580c652312b
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-05-16 23:08:14 +0200

    MiniMapCamera script comments

--- Assets/Scripts/MiniMapCamera.cs
+++ Assets/Scripts/MiniMapCamera.cs
@@ -1,15 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The MiniMapCamera class handles the positioning and rotation of the minimap camera.
+/// </summary>
 public class MiniMapCamera : MonoBehaviour
 {
+    /// <value><c>player</c> is the Transform of the player object that the minimap camera follows.</value>
     [SerializeField]
     private Transform player;
 
+    /// <summary>
+    /// Updating of the position and rotation of the minimap camera to follow the player.
+    /// </summary>
     void LateUpdate()
     {
+        // Set the position of the minimap camera to be above the player
         transform.position = player.position + new Vector3(0, 10, 0);
+
+        // Set the rotation of the minimap camera to match the player's y rotation
         transform.rotation = Quaternion.Euler(90, player.eulerAngles.y, 0);
     }
 }
