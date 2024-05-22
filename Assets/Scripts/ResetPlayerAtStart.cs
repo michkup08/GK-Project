d1970dcafbc66677fd0c7a255fc82f15fb324740
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-05-22 15:27:46 +0200

    ResetPlayerAtStart script comments

--- Assets/Scripts/ResetPlayerAtStart.cs
+++ Assets/Scripts/ResetPlayerAtStart.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The ResetPlayerAtStart class is responsible for resetting the player's state at the start of the game.
+/// It is needed for the player to be correctly moving when on moving platforms.
+/// </summary>
 public class ResetPlayerAtStart : MonoBehaviour
 {
+    /// <summary>
+    /// Activates and deactivates the player object at the start of the game.
+    /// </summary>
     void Start()
     {
         GameObject player = GameObject.Find("Player");
         player.SetActive(false);
-        player.SetActive(true);        
+        player.SetActive(true);
     }
 }
