2402f870fd45e6d0b45d570e3593e29dcec8cba0
michkup08 <91846445+michkup08@users.noreply.github.com>
2024-05-29 14:48:10 +0200

    system poruszania przeciwnika

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    private bool playerInRange;
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            float dist = Vector3.Distance(other.transform.position, transform.position);
+            if(dist>2)
+            {
+                playerInRange = true;
+                Debug.Log("Enemy set agro");
+                agent.SetDestination(playerTransform.position + (transform.position.normalized*1.5f));
+            }
+            
+        }
+    }
+}
