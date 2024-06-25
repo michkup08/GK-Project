f62ecf8e51279b20b0b2272a521dbc6bad1109a9
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-06-25 22:24:39 +0200

    PlayerInLift script comments

--- Assets/Scripts/PlayerInLift.cs
+++ Assets/Scripts/PlayerInLift.cs
@@ -1,19 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Manages the player's animation state when inside a lift, based on his movement.
+/// </summary>
 public class PlayerInLift : MonoBehaviour
 {
+    /// <value><c>playerAnimator</c> is used to control the player's animations.</value>
     [SerializeField]
     private Animator playerAnimator;
 
+    /// <value><c>player</c> refers to the player's Rigidbody component, used to check his movement.</value>
     [SerializeField]
     private Rigidbody player;
 
+    /// <summary>
+    /// Called when the player continuously collides with the lift.
+    /// It checks the player's movement and updates the animation state accordingly.
+    /// </summary>
+    /// <param name="collision">The Collision data associated with this collision.</param>
     private void OnCollisionStay(Collision collision)
     {
+        // Check if the collision is with the player
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            // If the player is moving, set 'inLift' to false, else to true
             if (player.velocity.x != 0 || player.velocity.z != 0)
             {
                 playerAnimator.SetBool("inLift", false);
@@ -25,10 +35,17 @@ public class PlayerInLift : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Called when the player exits the collision with the lift.
+    /// It resets the player's animation state to indicate they are not in the lift.
+    /// </summary>
+    /// <param name="collision">The Collision data associated with this collision.</param>
     private void OnCollisionExit(Collision collision)
     {
+        // Check if the collision is with the player
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            // Reset the 'inLift' animation state to false
             playerAnimator.SetBool("inLift", false);
         }
     }
