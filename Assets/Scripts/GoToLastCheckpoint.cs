a37b24e74ada585006c4f170028ec331c6bb0772
Gaikk <gaik.k02@gmail.com>
2024-06-26 23:00:11 +0200

    Update GoToLastCheckpoint.cs

--- Assets/Scripts/GoToLastCheckpoint.cs
+++ Assets/Scripts/GoToLastCheckpoint.cs
@@ -1,29 +1,41 @@
 using UnityEngine;
 
+/// <summary>
+/// This class handles resetting the player to the last checkpoint upon collision or trigger with specific objects.
+/// </summary>
 public class GoToLastCheckpoint : MonoBehaviour
 {
+    /// <value> Reference to the checkpoint manager. </value>
     public checkPointsMenager menager;
+    /// <value> Reference to the player game object. </value>
     public GameObject player;
+    /// <value> Reference to the Animator component. </value>
     public Animator animator;
 
+    /// <summary>
+    /// Handles the behavior when another collider enters the trigger.
+    /// </summary>
+    /// <param name="other">The Collider that triggered the event.</param>
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("reset");
 
-        if (other.tag == "Player")
+        if (other.CompareTag("Player"))
         {
             player.transform.position = menager.getPosition();
             Debug.Log("colission");
         }
-
-
     }
 
+    /// <summary>
+    /// Handles the behavior when a collision occurs.
+    /// </summary>
+    /// <param name="collision">The Collision that triggered the event.</param>
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("reset");
 
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
             if (!animator)
             {
@@ -42,5 +54,4 @@ public class GoToLastCheckpoint : MonoBehaviour
             }
         }
     }
-
 }
