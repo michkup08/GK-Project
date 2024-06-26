c0516f5b085861f5b45bada8df0d8b8fd1c223e3
Gaikk <gaik.k02@gmail.com>
2024-06-26 23:15:48 +0200

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// This class manages the main menu interactions such as starting the game and quitting the application.
+/// </summary>
 public class MainMenu : MonoBehaviour
 {
+    /// <summary>
+    /// Loads the main game scene.
+    /// </summary>
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
 
+    /// <summary>
+    /// Quits the application.
+    /// </summary>
     public void QuitGame()
     {
         Application.Quit();
