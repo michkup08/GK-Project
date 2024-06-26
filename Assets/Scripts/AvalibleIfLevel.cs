4da7097d2eccbe2f03fc8cabe5ecc17961e5b078
Gaikk <gaik.k02@gmail.com>
2024-06-26 22:37:59 +0200

    Update AvalibleIfLevel.cs

--- Assets/Scripts/AvalibleIfLevel.cs
+++ Assets/Scripts/AvalibleIfLevel.cs
@@ -1,19 +1,21 @@
 using UnityEngine;
 
+/// <summary>
+/// This class controls the activation of a GameObject based on the player's current level.
+/// </summary>
 public class AvalibleIfLevel : MonoBehaviour
 {
+    /// <value> The level required to activate the GameObject. </value>
     public int levelToActive;
+
+    /// <summary>
+    /// Initializes the GameObject and sets its active state based on the player's current level.
+    /// </summary>
     void Start()
     {
         if (SaveSystem.Load().currentLevel < levelToActive)
         {
             gameObject.SetActive(false);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
