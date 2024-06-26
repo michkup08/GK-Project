a5e283b45c312275269183a9fd8a45c401be45fa
Gaikk <gaik.k02@gmail.com>
2024-06-26 22:58:28 +0200

    Update FinishLevelBarrel.cs

--- Assets/Scripts/FinishLevelBarrel.cs
+++ Assets/Scripts/FinishLevelBarrel.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class handles the finish level logic when a barrel enters the trigger.
+/// </summary>
 public class FinishLevelBarrel : MonoBehaviour
 {
+    /// <summary>
+    /// Handles the behavior when another collider enters the trigger.
+    /// </summary>
+    /// <param name="collider">The Collider that triggered the event.</param>
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Barrel"))
